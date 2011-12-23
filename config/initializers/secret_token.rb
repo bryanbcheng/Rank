c15e91e507d429a97acea772b46a4fbd3b938c92
Bryan Cheng <bryan.b.cheng@gmail.com>
2011-12-22 16:44:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rank::Application.config.secret_token = '861d9ad9f994d3f80945a26c4370e8d75f9c0e8c4a8f7bdcda6f9c17ed35bcd88558ad8e762a5b43c207b753227275b22af27a0751b7f851ab52f43f7de043fe'
